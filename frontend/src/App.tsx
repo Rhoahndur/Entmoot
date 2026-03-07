cff3f88a808e9f57e0f802724a18bad1f2b0936d
rhoahndur <aleksandergaun@gmail.com>
2026-03-07 16:02:23 -0500

    add Vercel analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,29 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { UploadPage } from './pages/UploadPage';
-import { ConfigPage } from './pages/ConfigPage';
-import { ResultsPage } from './pages/ResultsPage';
-import { ProjectsListPage } from './pages/ProjectsListPage';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
+import { UploadPage } from "./pages/UploadPage";
+import { ConfigPage } from "./pages/ConfigPage";
+import { ResultsPage } from "./pages/ResultsPage";
+import { ProjectsListPage } from "./pages/ProjectsListPage";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/upload" replace />} />
-        <Route path="/upload" element={<UploadPage />} />
-        <Route path="/config" element={<ConfigPage />} />
-        <Route path="/results" element={<ResultsPage />} />
-        <Route path="/projects" element={<ProjectsListPage />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/upload" replace />} />
+          <Route path="/upload" element={<UploadPage />} />
+          <Route path="/config" element={<ConfigPage />} />
+          <Route path="/results" element={<ResultsPage />} />
+          <Route path="/projects" element={<ProjectsListPage />} />
+        </Routes>
+      </Router>
+      <Analytics />
+    </>
   );
 }
 
